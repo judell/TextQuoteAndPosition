1fbc8adf0fc7f68cb9280c4f772653917ef08639
judell <jon@jonudell.info>
2019-01-01 10:01:40 -0800

    also export WrapRangeText

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 var anchoring = {
   TextQuoteAnchor: require ('dom-anchor-text-quote'),
   TextPositionAnchor: require ('dom-anchor-text-position'),
+  WrapRangeText: require('wrap-range-text')
 }
 
 module.exports = anchoring;
